e795be3964e588838204451433fc4d0272231057
Myagoo <millagou.benjamin@gmail.com>
2014-09-22 22:26:30 +0200

    hey salut

---
+++ src/aa.jsx
@@ -0,0 +1,4 @@
+var React = require('react');
+var Datepicker = require('react-datepicker');
+
+React.renderComponent(<Datepicker></Datepicker>, document.body);
--- src/client.jsx
+++ src/client.jsx
@@ -1,4 +1,46 @@
 var React = require('react');
-var Alert = require('react-bootstrap/Alert');
+var DatePicker = require('react-datepicker-component/DatePicker.jsx');
+var DatePickerInput = require('react-datepicker-component/DatePickerInput.jsx');
 
-React.renderComponent(<Alert>Test</Alert>, document.body);
+var Selector = React.createClass({
+  getInitialState: function() {
+    return {
+      open: false
+    }
+  },
+  handleClick: function() {
+    this.setState({
+      open: !this.state.open
+    });
+  },
+  renderDropDown: function() {
+    if(this.state.open === false){
+      return undefined;
+    }
+
+    var items = React.Children.map(this.props.children, function (child) {
+      return <li>{child}</li>;
+    });
+
+    return <ul>{items}</ul>;
+  },
+  render: function() {
+    return (
+      <div>
+      <span onClick={this.handleClick}>Choisissez une valeur</span>
+      {this.renderDropDown()}
+      </div>
+    );
+  }
+});
+
+var items = [];
+
+for (var i = 0, j = 3 ; i < j ; i++) {
+  items.push(i);
+}
+items.push(<DatePicker date={new Date(2014, 0, 8)} />);
+
+React.renderComponent(<Selector><DatePickerInput
+        classNamePrefix={"wide-datepicker"}
+        date={new Date(2012, 0, 4)} /></Selector>, document.body);
