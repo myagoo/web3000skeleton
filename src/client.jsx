ba5a3e570c27348cb76d0c1a0df3c7bc52c94d24
Myagoo <millagou.benjamin@gmail.com>
2014-09-22 23:44:32 +0200

    testing selector composability

--- src/client.jsx
+++ src/client.jsx
@@ -1,11 +1,48 @@
 var React = require('react');
-var DatePicker = require('react-datepicker-component/DatePicker.jsx');
-var DatePickerInput = require('react-datepicker-component/DatePickerInput.jsx');
+var DatePicker = require('date-pikinator');
+
+var SelectableItem = React.createClass({
+  handleClick: function(){
+    this.props.onSelect && this.props.onSelect(this.props.value || this.props.children, this.props.children);
+  },
+  render: function(){
+    return <span onClick={this.handleClick}>{this.props.children}</span>
+  }
+});
+
+var SelectableDatepicker = React.createClass({
+  getInitialState: function(){
+    return {
+      value: this.props.value
+    };
+  },
+  handleValidate: function(){
+    this.props.onSelect && this.props.onSelect(this.state.value, this.state.value);
+  },
+  handleChange: function(e){
+    this.setState({
+      value: e.target.value
+    });
+  },
+  render: function(){
+    return (
+      <div>
+        <input
+          type="date"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleValidate}>'√'</button>
+      </div>
+    );
+  }
+});
 
 var Selector = React.createClass({
   getInitialState: function() {
     return {
-      open: false
+      open: false,
+      displayValue: 'Choisissez une valeur...'
     }
   },
   handleClick: function() {
@@ -13,34 +50,64 @@ var Selector = React.createClass({
       open: !this.state.open
     });
   },
-  renderDropDown: function() {
-    if(this.state.open === false){
-      return undefined;
-    }
-
-    var items = React.Children.map(this.props.children, function (child) {
-      return <li>{child}</li>;
+  handleSelect: function(formValue, displayValue){
+    this.setState({
+      value: formValue,
+      displayValue: displayValue,
+      open: false
     });
 
-    return <ul>{items}</ul>;
+    this.props.onChange && this.props.onChange(formValue, displayValue);
+  },
+  getDropDownStyle: function(){
+    return {
+      display: this.state.open ? 'block': 'none',
+      position: 'absolute'
+    };
+  },
+  renderDropDown: function() {
+    var items = this.props.children.map(function (child, index) {
+      child.props.onSelect = this.handleSelect;
+      return <li key={index}>{child}</li>;
+    }.bind(this));
+
+    return <ul style={this.getDropDownStyle()}>{items}</ul>;
   },
   render: function() {
     return (
       <div>
-      <span onClick={this.handleClick}>Choisissez une valeur</span>
-      {this.renderDropDown()}
+        <span onClick={this.handleClick}>{this.state.displayValue}</span>
+        {this.renderDropDown()}
       </div>
     );
+  }
+});
+
+var SelectableSelector = React.createClass({
+  handleChange: function(formValue, displayValue){
+    this.props.onSelect && this.props.onSelect(formValue, displayValue);
+  },
+  render: function(){
+    return this.transferPropsTo(<Selector onChange={this.handleChange}>{this.props.children}</Selector>);
   }
 });
 
 var items = [];
 
 for (var i = 0, j = 3 ; i < j ; i++) {
-  items.push(i);
+  items.push(<SelectableItem value={i}>{'Item #' + i}</SelectableItem>);
 }
-items.push(<DatePicker date={new Date(2014, 0, 8)} />);
 
-React.renderComponent(<Selector><DatePickerInput
-        classNamePrefix={"wide-datepicker"}
-        date={new Date(2012, 0, 4)} /></Selector>, document.body);
+items.push(<SelectableDatepicker value="22/09/2014"/>);
+
+var items2 = [];
+
+for (var i = 3, j = 6 ; i < j ; i++) {
+  items2.push(<SelectableItem value={i}>{'Item #' + i}</SelectableItem>);
+}
+
+items2.push(<SelectableDatepicker value="23/09/2014"/>);
+
+items.push(<SelectableSelector>{items2}</SelectableSelector>);
+
+React.renderComponent(<Selector>{items}</Selector>, document.body);
